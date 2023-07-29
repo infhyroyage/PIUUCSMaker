7b1621ef0e84f825e083d936114294b47aba7fe4
Takeru KIDO <keidy2463@gmail.com>
2023-07-29 17:52:24 +0900

    Add transition rendering chart

--- src/components/UploadRequest.tsx
+++ src/components/UploadRequest.tsx
@@ -1,6 +1,55 @@
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import { useSetRecoilState } from "recoil";
+import {
+  isShownSystemErrorSnackbarState,
+  isShownTopBarProgressState,
+} from "../service/atoms";
+import { useState, useTransition } from "react";
+import { Block, Chart, Note } from "../types/ucs";
+
+const validateAndLoadUCS = (content: string): Chart => {
+  // TODO: UCSファイル解析(ダミーの長時間処理)
+  console.log(content);
+  const blocks = Array(100)
+    .fill("")
+    .map<Block>((_, i) => {
+      return {
+        bpm: i,
+        delay: i,
+        beat: i,
+        split: i,
+        length: i,
+        notes: Array(1000)
+          .fill("")
+          .map<Note>((_, j) => {
+            return {
+              column: j % 10,
+              start: j,
+              goal: j,
+              hollowStarts: [],
+              hollowGoals: [],
+            };
+          }),
+      };
+    });
+  return {
+    length: 5,
+    isPerformance: true,
+    blocks,
+  };
+};
 
 function UploadRequest() {
+  const [chart, setChart] = useState<Chart | undefined>(undefined);
+  const setIsShownProgress = useSetRecoilState<boolean>(
+    isShownTopBarProgressState
+  );
+  const setIsShownSystemErrorSnackbar = useSetRecoilState<boolean>(
+    isShownSystemErrorSnackbarState
+  );
+
+  const [isPending, startTransition] = useTransition();
+
   const handleUploadUCS = (event: React.ChangeEvent<HTMLInputElement>) => {
     // UCSファイルを何もアップロードしなかった場合はNOP
     const fileList: FileList | null = event.target.files;
@@ -8,28 +57,48 @@ function UploadRequest() {
 
     const reader: FileReader = new FileReader();
     reader.onload = () => {
-      // アップロードしたUCSがstring型だとみなされない場合はアップロードエラー
-      if (typeof reader.result === "string") {
-        // TODO
-        console.log(reader.result);
-      } else {
-        // TODO
+      // 内部矛盾チェック
+      if (typeof reader.result !== "string") {
         console.error(typeof reader.result);
+        setIsShownSystemErrorSnackbar(true);
+        return;
       }
+      const content: string = reader.result;
+
+      // 譜面以外のDOMを高優先度で再レンダリング
+      setIsShownProgress(true);
+
+      // 譜面のDOMを低優先度で再レンダリング
+      startTransition(() => {
+        setChart(validateAndLoadUCS(content));
+        setIsShownProgress(false);
+      });
     };
     reader.readAsText(fileList[0]);
   };
 
-  return (
-    <Button variant="contained" component="label">
-      ucsファイルをアップロード
-      <input
-        type="file"
-        accept=".ucs"
-        style={{ display: "none" }}
-        onChange={handleUploadUCS}
-      />
-    </Button>
+  return chart ? (
+    // TODO: 譜面のレンダリング
+    chart.blocks.map((block, idx) => (
+      <Box key={idx}>{JSON.stringify(block.notes)}</Box>
+    ))
+  ) : (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Button variant="contained" component="label" disabled={isPending}>
+        {isPending ? "読み込み中..." : "ucsファイルをアップロード"}
+        <input
+          type="file"
+          accept=".ucs"
+          style={{ display: "none" }}
+          onChange={handleUploadUCS}
+        />
+      </Button>
+    </Box>
   );
 }
 
