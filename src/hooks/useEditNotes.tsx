d97c08114cce848c7293bac565e24aebfd4c65cf
Takeru KIDO <keidy2463@gmail.com>
2023-08-21 22:29:07 +0900

    Add setting hold

--- src/hooks/useEditNotes.tsx
+++ src/hooks/useEditNotes.tsx
@@ -57,60 +57,84 @@ function useEditNotes() {
       mouseDownRowIdx < mouseUpRowIdx ? mouseDownRowIdx : mouseUpRowIdx;
     const goal: number =
       mouseDownRowIdx < mouseUpRowIdx ? mouseUpRowIdx : mouseDownRowIdx;
-    // start/goalに対応する譜面のブロックのインデックスを取得
-    const startBlocklIdx: number =
+    // startに対応する譜面のブロックのインデックスを取得
+    const startBlockIdx: number =
       mouseDownBlockIdx < mouseUpBlockIdx ? mouseDownBlockIdx : mouseUpBlockIdx;
-    const goalBlocklIdx: number =
-      mouseDownBlockIdx < mouseUpBlockIdx ? mouseUpBlockIdx : mouseDownBlockIdx;
 
-    let updatedStartBlockIdxNotes: Note[] = [
-      ...chart.blocks[startBlocklIdx].notes[column],
-    ];
+    let updatedChart: Chart;
     if (start === goal) {
-      // startの場所に単ノートを追加
-      // ただし、その場所に単ノート/(中抜き)ホールドが含む場合は追加せず、それを削除する
-      const noteIdx: number = chart.blocks[startBlocklIdx].notes[
+      let updatedNotes: Note[] = [...chart.blocks[startBlockIdx].notes[column]];
+
+      // startの場所に単ノートを新規追加
+      // ただし、その場所に単ノート/(中抜き)ホールドが含む場合は、それを削除する(単ノートは新規追加しない)
+      const noteIdx: number = chart.blocks[startBlockIdx].notes[
         column
       ].findIndex((note: Note) => note.start <= start && start <= note.goal);
       if (noteIdx === -1) {
-        updatedStartBlockIdxNotes.push({
+        updatedNotes.push({
           start,
           goal,
           hollowStarts: [],
           hollowGoals: [],
         });
       } else {
-        updatedStartBlockIdxNotes = [
-          ...chart.blocks[startBlocklIdx].notes[column].slice(0, noteIdx),
-          ...chart.blocks[startBlocklIdx].notes[column].slice(noteIdx + 1),
+        updatedNotes = [
+          ...chart.blocks[startBlockIdx].notes[column].slice(0, noteIdx),
+          ...chart.blocks[startBlockIdx].notes[column].slice(noteIdx + 1),
         ];
       }
+
+      updatedChart = {
+        ...chart,
+        blocks: chart.blocks.map((block: Block, blockIdx: number) =>
+          blockIdx === startBlockIdx
+            ? {
+                ...block,
+                notes: block.notes.map((notes: Note[], j: number) =>
+                  j === column ? updatedNotes : notes
+                ),
+              }
+            : block
+        ),
+      };
     } else {
-      // TODO: startとgoalとの間にホールドを追加
-      // TODO: 中抜きホールドの追加は未実装
-      console.log({
-        start,
-        goal,
-        startBlocklIdx,
-        goalBlocklIdx,
-      });
-      return;
+      // goalに対応する譜面のブロックのインデックスを取得
+      const goalBlockIdx: number =
+        mouseDownBlockIdx < mouseUpBlockIdx
+          ? mouseUpBlockIdx
+          : mouseDownBlockIdx;
+
+      // startとgoalとの間にホールドを新規追加
+      // ただし、その間の場所に単ノート/(中抜き)ホールドが含む場合は、それをすべて削除してから新規追加する
+      updatedChart = {
+        ...chart,
+        blocks: chart.blocks.map((block: Block, blockIdx: number) => {
+          if (blockIdx < startBlockIdx || blockIdx > goalBlockIdx) return block;
+
+          const updatedNotes: Note[] = chart.blocks[blockIdx].notes[
+            column
+          ].filter((note: Note) => note.start > goal || note.goal < start);
+          if (blockIdx === startBlockIdx) {
+            updatedNotes.push({
+              start,
+              goal,
+              hollowStarts: [],
+              hollowGoals: [],
+            });
+          }
+
+          return {
+            ...block,
+            notes: block.notes.map((notes: Note[], j: number) =>
+              j === column ? updatedNotes : notes
+            ),
+          };
+        }),
+      };
     }
 
     // 譜面の更新
-    setChart({
-      ...chart,
-      blocks: chart.blocks.map((block: Block, i: number) =>
-        i === startBlocklIdx
-          ? {
-              ...block,
-              notes: block.notes.map((columnNotes: Note[], j: number) =>
-                j === column ? updatedStartBlockIdxNotes : columnNotes
-              ),
-            }
-          : block
-      ),
-    });
+    setChart(updatedChart);
   };
 
   return { editNotes };
