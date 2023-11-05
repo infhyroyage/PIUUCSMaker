7a0986d022eee80f81b3f3d03a498febab2b3926
Takeru KIDO <keidy2463@gmail.com>
2023-11-05 16:07:58 +0900

    Update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,13 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
+  collectCoverage: true,
+  coverageReporters: ["text"],
+  coverageThreshold: {
+    global: {
+      lines: 80,
+    },
+  },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   testEnvironment: "jsdom",
   transform: { "^.+.(ts|tsx)$": "ts-jest" },
