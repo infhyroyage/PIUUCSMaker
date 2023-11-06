50d024267a266ede2daea58d731d1b3f693e0c62
Takeru KIDO <keidy2463@gmail.com>
2023-11-07 00:02:57 +0900

    Update NewUCSDialog test

--- __tests__/components/dialog/NewUCSDialog.spec.tsx
+++ __tests__/components/dialog/NewUCSDialog.spec.tsx
@@ -1,9 +1,10 @@
 import "@testing-library/jest-dom";
-import { fireEvent, render, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { RecoilRoot, useSetRecoilState } from "recoil";
 import NewUCSDialog from "../../../src/components/dialog/NewUCSDialog";
 import { isOpenedNewUCSDialogState } from "../../../src/services/atoms";
 import { Block, ChartSnapshot, Note } from "../../../src/types/ucs";
+import userEvent from "@testing-library/user-event";
 
 const mockSetBlocks = jest.fn();
 const mockSetIsPerformance = jest.fn();
@@ -51,6 +52,50 @@ describe("NewUCSDialog", () => {
     expect(getByLabelText("Split")).toHaveValue(2);
     expect(getByLabelText("Beat")).toHaveValue(4);
     expect(getByLabelText("Rows")).toHaveValue(50);
+  });
+
+  it("Rerender all inputs changed different values", async () => {
+    const { getByLabelText } = render(
+      <RecoilRoot
+        initializeState={({ set }) => {
+          set(isOpenedNewUCSDialogState, true);
+        }}
+      >
+        <NewUCSDialog />
+      </RecoilRoot>
+    );
+
+    fireEvent.change(getByLabelText("UCS File Name"), {
+      target: { value: "foo" },
+    });
+    await userEvent.click(getByLabelText("Mode"));
+    const item = await screen.findByText("Double Performance");
+    await userEvent.click(item);
+    fireEvent.change(getByLabelText("BPM"), {
+      target: { value: "123.4567" },
+    });
+    fireEvent.change(getByLabelText("Delay(ms)"), {
+      target: { value: "0.1234567" },
+    });
+    fireEvent.change(getByLabelText("Split"), {
+      target: { value: "4" },
+    });
+    fireEvent.change(getByLabelText("Beat"), {
+      target: { value: "3" },
+    });
+    fireEvent.change(getByLabelText("Rows"), {
+      target: { value: "10" },
+    });
+
+    await waitFor(() => {
+      expect(getByLabelText("UCS File Name")).toHaveValue("foo");
+      expect(getByLabelText("Mode")).toHaveTextContent("Double Performance");
+      expect(getByLabelText("BPM")).toHaveValue(123.4567);
+      expect(getByLabelText("Delay(ms)")).toHaveValue(0.1234567);
+      expect(getByLabelText("Split")).toHaveValue(4);
+      expect(getByLabelText("Beat")).toHaveValue(3);
+      expect(getByLabelText("Rows")).toHaveValue(10);
+    });
   });
 
   it("Render invisibly if CANCEL button is clicked", async () => {
@@ -119,16 +164,15 @@ describe("NewUCSDialog", () => {
   });
 
   it("Render visibly with errors if CREATE button is clicked with invalid 6 inputs", async () => {
-    const { findByText, getByRole, getByLabelText, getByText, queryByRole } =
-      render(
-        <RecoilRoot
-          initializeState={({ set }) => {
-            set(isOpenedNewUCSDialogState, true);
-          }}
-        >
-          <NewUCSDialog />
-        </RecoilRoot>
-      );
+    const { getByLabelText, getByText, queryByRole } = render(
+      <RecoilRoot
+        initializeState={({ set }) => {
+          set(isOpenedNewUCSDialogState, true);
+        }}
+      >
+        <NewUCSDialog />
+      </RecoilRoot>
+    );
 
     fireEvent.change(getByLabelText("UCS File Name"), {
       target: { value: "" },
