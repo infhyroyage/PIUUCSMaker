93d050086730415c0aa41589bf013535f7b14465
Takeru KIDO <keidy2463@gmail.com>
2023-11-07 22:58:29 +0900

    Update useSelectedDeleting test

--- __tests__/hooks/useSelectedDeleting.spec.tsx
+++ __tests__/hooks/useSelectedDeleting.spec.tsx
@@ -1,4 +1,4 @@
-import { act, renderHook } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import useSelectedDeleting from "../../src/hooks/useSelectedDeleting";
 
@@ -18,7 +18,7 @@ describe("useSelectedDeleting", () => {
     jest.resetAllMocks();
   });
 
-  it("Do nothing if selector.completed is null", async () => {
+  it("Do nothing if selector.completed is null", () => {
     (useRecoilState as jest.Mock).mockReturnValueOnce([[], mockSetNotes]);
     (useRecoilState as jest.Mock).mockReturnValueOnce([
       [],
@@ -30,17 +30,92 @@ describe("useSelectedDeleting", () => {
 
     const { result } = renderHook(useSelectedDeleting);
     const { handleDelete } = result.current;
-    act(() => {
-      handleDelete();
-
-      // Trigger the Delete key event.
-      // const event = new KeyboardEvent("keydown", { key: "Delete" });
-      // document.dispatchEvent(event);
-    });
+    act(() => handleDelete());
 
     expect(mockSetIsProtected).not.toHaveBeenCalled();
     expect(mockSetNotes).not.toHaveBeenCalled();
     expect(mockSetUndoSnapshots).not.toHaveBeenCalled();
     expect(mockSetRedoSnapshots).not.toHaveBeenCalled();
+  });
+
+  it("Delete notes only with selected area", () => {
+    (useRecoilState as jest.Mock).mockReturnValueOnce([
+      Array(5).fill([...Array(4)].map((_, i) => ({ rowIdx: i, type: "X" }))),
+      mockSetNotes,
+    ]);
+    (useRecoilState as jest.Mock).mockReturnValueOnce([
+      [
+        {
+          blocks: [
+            {
+              accumulatedRows: 0,
+              beat: 4,
+              bpm: 120,
+              delay: 0,
+              rows: 50,
+              split: 2,
+            },
+          ],
+          notes: null,
+        },
+      ],
+      mockSetUndoSnapshots,
+    ]);
+    (useRecoilValue as jest.Mock).mockReturnValueOnce({
+      completed: {
+        goalColumn: 3,
+        goalRowIdx: 2,
+        startColumn: 1,
+        startRowIdx: 1,
+      },
+    });
+    (useSetRecoilState as jest.Mock).mockReturnValueOnce(mockSetIsProtected);
+    (useSetRecoilState as jest.Mock).mockReturnValueOnce(mockSetRedoSnapshots);
+
+    const { result } = renderHook(useSelectedDeleting);
+    const { handleDelete } = result.current;
+    act(() => {
+      handleDelete();
+    });
+
+    expect(mockSetIsProtected).toHaveBeenCalledWith(true);
+    expect(mockSetNotes).toHaveBeenCalledWith([
+      [...Array(4)].map((_, i) => ({ rowIdx: i, type: "X" })),
+      [
+        { rowIdx: 0, type: "X" },
+        { rowIdx: 3, type: "X" },
+      ],
+      [
+        { rowIdx: 0, type: "X" },
+        { rowIdx: 3, type: "X" },
+      ],
+      [
+        { rowIdx: 0, type: "X" },
+        { rowIdx: 3, type: "X" },
+      ],
+      [...Array(4)].map((_, i) => ({ rowIdx: i, type: "X" })),
+    ]);
+    expect(mockSetUndoSnapshots).toHaveBeenCalledWith([
+      {
+        blocks: [
+          {
+            accumulatedRows: 0,
+            beat: 4,
+            bpm: 120,
+            delay: 0,
+            rows: 50,
+            split: 2,
+          },
+        ],
+        notes: null,
+      },
+      {
+        blocks: null,
+        notes: Array(5).fill(
+          [...Array(4)].map((_, i) => ({ rowIdx: i, type: "X" }))
+        ),
+      },
+    ]);
+    expect(mockSetRedoSnapshots).toHaveBeenCalledWith([]);
   });
 });
