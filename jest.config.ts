b9e3547d27bdb888854059ff3fa5ae6ead800cb3
Takeru KIDO <keidy2463@gmail.com>
2023-11-12 17:37:31 +0900

    Add GitHub Actions Reporter of jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,16 +2,15 @@ import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   collectCoverage: true,
-  coverageReporters: ["text"],
   coverageThreshold: {
     global: {
       statements: 80,
     },
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  reporters: [["github-actions", { silent: false }], "summary"],
   testEnvironment: "jsdom",
   transform: { "^.+.(ts|tsx)$": "ts-jest" },
-  verbose: true,
 };
 
 export default config;
