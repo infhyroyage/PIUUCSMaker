f0aeb7671a0eef92a5bd65581512e83ffe30da9b
Takeru KIDO <keidy2463@gmail.com>
2024-06-21 00:45:10 +0900

    Install tailwindcss and daisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+};
