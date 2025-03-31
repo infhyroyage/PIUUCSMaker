2653c700dbac98d198a7fbf6b5ef5870e8fd222e
Takeru KIDO <keidy2463@gmail.com>
2025-04-01 00:19:16 +0900

    Remove daisyui from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
-import daisyui from "daisyui";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {},
-  },
-  plugins: [daisyui],
-  daisyui: {
-    themes: ["light", "dark"],
   },
 };
